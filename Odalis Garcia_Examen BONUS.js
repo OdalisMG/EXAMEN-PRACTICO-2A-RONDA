8a843de49df3a1bb4d8cf3e0d47b8b5c662506e3
Odamg <godalis64@gmail.com>
2022-09-08 19:48:55 -0500

    8/9/2022

---
+++ Odalis Garcia_Examen BONUS.js
@@ -0,0 +1,14 @@
+var str = 'https://purchase.hotmart.com/purchase';
+    
+var str_array = str.split('.');
+str_array[0] = str_array[0].toUpperCase();
+
+str = '';
+str_array.forEach(unir);
+
+function unir(item, index){
+    if(str != '') str += '.';
+    str += item;
+}
+
+console.log(str);
