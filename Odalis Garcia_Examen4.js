eb03e242dd539b47fff99342a7bed0a698b5cb12
Odamg <godalis64@gmail.com>
2022-09-08 20:20:41 -0500

    8/9/2022

---
+++ Odalis Garcia_Examen4.js
@@ -0,0 +1,9 @@
+const array1 = ['nace', 'crece', 'muere'];
+console.log('arreglo original:', array1);
+// expected output: "array1:" Array ["nace", "crece", "muere"]
+
+const reversed = array1.reverse();
+console.log('arreglo invertido:', reversed);
+
+
+
